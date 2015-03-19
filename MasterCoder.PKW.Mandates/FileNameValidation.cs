c79af45e0a4592d5392a602d492986b39d0e523a
Michał Kuliński <michal.kulinski@accenture.com>
2015-03-19 13:37:31 +0100

    poprawa działania validacji IP - regex jest błędny i zbyt wolno działą dla takich zastosowań

--- MasterCoder.PKW.Mandates/FileNameValidation.cs
+++ MasterCoder.PKW.Mandates/FileNameValidation.cs
@@ -1,17 +1,17 @@
-﻿using System.Text.RegularExpressions;
-
+﻿using System.Net;
 //ograniczenie nadmiarowych using'ów
 
 namespace MasterCoder.PKW.Mandates
 {
     public class FileNameValidation
     {
-        private static readonly string p = @"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+        //private static readonly string p = @"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
 
+        //poprawa działania validacji numeru IP - metoda wykorzystująca powyższy regex jest wolniejsza niz użycie standardowej metody bibliotecznej
         public static bool Validate(string n)
         {
-            n = n.Substring(0,n.Length - 4);
-            return Regex.IsMatch(n, p);
+            IPAddress ip;
+            return !string.IsNullOrEmpty(n) && IPAddress.TryParse(n, out ip);
         }
     }
 }
--- MasterCoder.PKW.Tests/UnitTest1.cs
+++ MasterCoder.PKW.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Net;
+using MasterCoder.PKW.Mandates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MasterCoder.PKW.Tests
 {
@@ -6,8 +9,40 @@ namespace MasterCoder.PKW.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void file_name_validation_test()
         {
+            const int maxComponent = 255;
+            for (int c1 = 0; c1 <= maxComponent; ++c1)
+                for (int c2 = 0; c2 <= maxComponent; ++c2)
+                    for (int c3 = 0; c3 <= maxComponent; ++c3)
+                        for (int c4 = 0; c4 <= maxComponent; ++c4)
+                        {
+                            var ipNumber = String.Format("{0}.{1}.{2}.{3}", c1, c2, c3, c4);
+                            Assert.IsTrue(FileNameValidation.Validate(ipNumber),
+                                String.Format("IP number {0} wasn't validated", ipNumber));
+                        }
+        }
+
+        [TestMethod]
+        public void file_name_validation_test_2()
+        {
+            const int maxComponent = 50;
+            for (int c1 = 0; c1 <= maxComponent; ++c1)
+                for (int c2 = 0; c2 <= maxComponent; ++c2)
+                    for (int c3 = 0; c3 <= maxComponent; ++c3)
+                        for (int c4 = 0; c4 <= maxComponent; ++c4)
+                        {
+                            var ipNumber = String.Format("{0}.{1}.{2}.{3}", c1, c2, c3, c4);
+                            Assert.IsTrue(IsValidIp(ipNumber),
+                                String.Format("IP number {0} wasn't validated", ipNumber));
+                        }
+        }
+
+        public bool IsValidIp(string addr)
+        {
+            IPAddress ip;
+            bool valid = !string.IsNullOrEmpty(addr) && IPAddress.TryParse(addr, out ip);
+            return valid;
         }
     }
 }
