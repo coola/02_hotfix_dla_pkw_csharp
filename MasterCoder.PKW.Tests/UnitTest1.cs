0416ccd382117242fb1060bf111a5abfe9d1f65e
Michał Kuliński <michal.kulinski@accenture.com>
2015-03-20 11:25:46 +0100

    test for input parameters validator

--- MasterCoder.PKW.Tests/UnitTest1.cs
+++ MasterCoder.PKW.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using MasterCoder.PKW.Mandates;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,6 +10,7 @@ namespace MasterCoder.PKW.Tests
     public class UnitTest1
     {
         [TestMethod]
+        [Ignore]
         public void file_name_validation_test()
         {
             const int maxComponent = 255;
@@ -21,5 +24,65 @@ namespace MasterCoder.PKW.Tests
                                 String.Format("IP number {0} wasn't validated", ipNumber));
                         }
         }
+
+
+
+        internal class InputParametersException : Exception
+        {
+            public InputParametersException(string message)
+                : base(message)
+            {
+            }
+        }
+
+        private static void ValidateInputParameters(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                throw new InputParametersException("Parameters count error.");
+            }
+
+            uint test;
+            if (!UInt32.TryParse(args[0], out test) || !UInt32.TryParse(args[1], out test))
+            {
+                //poprawa literówki w opisie błędu
+                throw new InputParametersException("Parameter should be digit.");
+            }
+
+            if (string.IsNullOrWhiteSpace(args[2]) || !Directory.Exists(args[2]))
+            {
+                throw new InputParametersException("Wrong path name.");
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InputParametersException))]
+        public void input_parameters_validation_test()
+        {
+
+            ValidateInputParameters(new string[] { });
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InputParametersException))]
+        public void input_parameters_validation_test_2()
+        {
+            ValidateInputParameters(new [] { "","",""});
+        }
+
+        [TestMethod]
+        public void input_parameters_validation_test_3()
+        {
+            ValidateInputParameters(new[] { "4", "1", "c:\\data" });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InputParametersException))]
+        public void input_parameters_validation_test_4()
+        {
+            ValidateInputParameters(new[] { "-4", "-1", "c:\\data" });
+        }
+
     }
 }
